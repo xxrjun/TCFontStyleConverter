ec4899c7919b2a3d0f300f7b518f750121a834ae
YCHM0304 <samsamsam876@gmail.com>
2024-03-26 16:40:35 +0800

    add .sh

---
+++ datasets/download_font_dataset.sh
@@ -0,0 +1,29 @@
+# 設置一個函數來檢查上一個命令是否成功執行
+check_status() {
+  if [ $? -ne 0 ]; then
+    echo "Error: $1"
+    exit 1
+  fi
+}
+
+# 調用Python腳本並評估其輸出以設置環境變量
+eval $(python load_env.py)
+
+
+# 使用環境變量中的ACCESS_TOKEN克隆數據集存儲庫
+echo "Cloning the dataset repository..."
+git clone https://YCHM0304:${ACCESS_TOKEN}@github.com/YCHM0304/FontHandWriting_Dataset_CycleGAN.git
+check_status "Failed to clone the dataset repository."
+
+# 檢查數據集目錄是否存在，如果不存在則退出
+if [ ! -d "./FontHandWriting_Dataset_CycleGAN" ]; then
+  echo "Dataset directory not found."
+  exit 1
+fi
+
+# 移動數據集目錄到正確的位置
+echo "Moving dataset directory..."
+mv ./FontHandWriting_Dataset_CycleGAN ./datasets/FontHandWriting_Dataset_CycleGAN
+check_status "Failed to move the dataset directory."
+
+echo "Dataset cloning and moving completed successfully."
