649ffadfa7eb3b9ebf79fe3ffce16bc8dd2b0de9
YCHM0304 <samsamsam876@gmail.com>
2024-04-29 22:18:40 +0800

    modify script

--- scripts/run_font2font.sh
+++ scripts/run_font2font.sh
@@ -3,8 +3,8 @@ src_font=$2
 dst_font=$3
 label=$4
 
-python font2img.py --src_font ./charset/src/{$src_font}.ttf \
-                   --dst_font ./charset/dst/{$dst_font}.ttf \
+python font2img.py --src_font ./charset/src/$src_font.ttf \
+                   --dst_font ./charset/dst/$dst_font.ttf \
                    --charset CN \
                    --sample_count $sample_count \
                    --sample_dir ./examples \
